0849f5dde821036a3b74f966819c3fd37ced8b55
Jeff Lyons <jeffreylyons@outlook.com>
2022-04-18 04:40:29 -0400

    deploy 9

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter basename="/portfolio">
+   
     <div className="App">
     <div>
           <Home />
@@ -38,7 +38,7 @@ function App() {
         <Footer />
       </div>
     </div>
-    </BrowserRouter>
+   
   );
 }
 

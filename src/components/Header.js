a0e53197288990a268a09704bda3e1326bdca78b
Jeff Lyons <jeffreylyons@outlook.com>
2022-04-18 17:48:25 -0400

    v1.0.3

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import '../styles/header.css'
+
+
+function Header() {
+  return (
+    <div className='header-container'>
+        <img className='profile-pic' src='https://res.cloudinary.com/redhero/image/upload/v1649819797/Donuts/11IMG_2794_lqemay.jpg' />
+        <ul> 
+
+        <Link className='header-link' to='/'>
+            <li>Home</li>
+          </Link>
+            <br></br>
+
+          <Link className='header-link' to='/aboutme'>
+            <li>About Me</li>
+          </Link>
+            <br></br>
+
+          <Link className='header-link' to='/education'>
+            <li>Education</li>
+          </Link>
+            <br></br>
+
+            <Link className='header-link' to='/portfolio'>
+            <li>Portfolio</li>
+            </Link>
+            <br></br>
+
+            <Link className='header-link' to='/contact'>
+            <li>Contact</li>
+            </Link>
+            <br></br>
+
+        </ul>
+
+    </div>
+  )
+}
+
+export default Header

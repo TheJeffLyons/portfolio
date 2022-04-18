1e604e235e0d0f2bdf3cbdb81997bc4f9670b2b7
Jeff Lyons <jeffreylyons@outlook.com>
2022-04-18 03:30:18 -0400

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, HashRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes, HashRouter, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer"
 import Home from "./components/Home";

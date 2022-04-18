174e70cac7ff51b34119dc33d193bb35fcca631a
Jeff Lyons <jeffreylyons@outlook.com>
2022-04-18 04:42:55 -0400

    deploy 10

--- src/index.js
+++ src/index.js
@@ -9,10 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <App /> }>
-        </Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );

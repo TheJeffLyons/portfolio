ce88bdbe9392210db2351b52a187ab4618ad1985
Jeff Lyons <jeffreylyons@outlook.com>
2022-04-21 17:59:42 -0400

    v2.2.5

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -3,7 +3,7 @@ import '../styles/topbanner.css'
 
 function TopBanner() {
   return (
-    <div className='top-banner'><h3 className='banner-text'>&#8594;Full Stack Web Developer&#8592;</h3></div>
+    <div className='top-banner'><h3 className='banner-text'>&#60;FullStackWebDeveloper /&#62;</h3></div>
   )
 }
 

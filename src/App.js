a68587649c294bb72b75b35c5b440780a59fe733
Jeff Lyons <jeffreylyons@outlook.com>
2022-04-21 14:56:59 -0400

    mobile version rollout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
-import Home from "./components/Home";
 import AboutMe from "./components/AboutMe";
 import Education from "./components/Education";
 import Portfolio from "./components/Portfolio";

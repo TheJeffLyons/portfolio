ce5d7d256827cba86b618c273c08a15a297e0743
Jeff Lyons <jeffreylyons@outlook.com>
2022-04-21 06:19:29 -0400

    v2.0.4

---
+++ src/components/OpenToWork.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/opentowork.css'
+
+function OpenToWork() {
+  return (
+    <div className='work-container'><h5>Open To Work</h5></div>
+  )
+}
+
+export default OpenToWork

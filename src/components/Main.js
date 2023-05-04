bd6722d7a77a20609dbe47f20931bc95b6d8002c
Jeff Lyons <jeffreylyons@outlook.com>
2023-05-04 01:32:43 -0500

    5/4/2023

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "../App.css";
 import ComingSoon from "./ComingSoon";
-import BubbleGame from "./BubbleGame";
+
 
 function Main() {
   return (
     <div className="app-container">
       <div>
         <ComingSoon />
-       <BubbleGame />
+       
       </div>
 
       <div></div>
